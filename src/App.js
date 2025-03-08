06ce06c7ce7f956d74aad01a6276f7a0fa2dcc09
tubosun <olatubosundiyaolu@gmail.com>
2025-03-08 21:25:13 +0100

    update to hamburger icon

--- src/App.js
+++ src/App.js
@@ -114,14 +114,14 @@ function App() {
               <MetaBalls
               color="#ffffff"
               cursorBallColor="#ffffff"
-              cursorBallSize={2}
+              cursorBallSize={.5}
               ballCount={15}
               animationSize={10}
               enableMouseInteraction={true}
               enableTransparency={true}
               hoverSmoothness={0.05}
               clumpFactor={2}
-              speed={0.3}
+              speed={0.15}
             />
 
               <TrueFocus />
